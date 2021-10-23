af56d5f828c91d07728a2ef1eb5601c0a41660d5
LuizDeSa <luizdesa2012@gmail.com>
2021-10-23 14:17:11 -0300

    Forms (template driven) Controles ngForm, ngSubmit e ngModel com two way data binding

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  usuario: any = {
+    nome: 'Luiz Alberto',
+    email: 'luiz@email.com'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
@@ -14,5 +19,6 @@ export class TemplateFormComponent implements OnInit {
 
   onSubmit(form: any){
     console.log(form);
+    console.log(this.usuario);
   }
 }
